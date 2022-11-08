00574715d709050b20cab691896b82c25042e015
Dmitriy Iskra <dmitriyiskra@mail.ru>
2022-11-08 19:26:21 +0300

    the task objects has been completed

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,35 @@
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name,
+    this.gender = gender,
+    this.age = age
+};
 
-}
+const studentFirst = new Student('Svetlana', 'woman', 25);
+const studentSecond = new Student('Igor', 'man', 23);
+const studentThird = new Student('Oleg', 'man', 27);
+const studentFourth = new Student('Irina', 'woman', 22);
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
 
+Student.prototype.addMark = function (mark) {
+  this?.marks ? this.marks.push(mark) : this.marks = [mark];
+};
+
+Student.prototype.addMarks = function (...mark) {
+  this?.marks ? this.marks.push(...mark) : this.marks = [...mark];
+};
+
+Student.prototype.getAverage = function () {
+  return this.marks.reduce( (acc, e) => acc += e ) / this.marks.length;
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+
+  this.excluded = reason;
+};
 // ваш код для остальных методов
