93ae76fae10aae5900851f4124721e4ca289294c
Dmitriy Iskra <dmitriyiskra@mail.ru>
2022-11-16 20:47:41 +0300

    task completed classes

---
+++ 5.classes/task.js
@@ -0,0 +1,144 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+
+    fix() {
+        this.state *= 1.5;
+    }
+
+    set state(condition) {
+        if(condition <= 0) {
+            this._state = 0;
+        }
+        else if(condition >= 100) {
+            this._state = 100;
+        }
+        else {
+            this._state = condition;
+        }
+    }
+
+    get state() {
+        return this._state
+    }
+};
+
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = 'magazine';
+    }
+}
+
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = 'book';
+    }
+}
+
+
+class NovelBook extends Book {
+    constructor(name, releaseDate, pagesCount, author) {
+        super(name, releaseDate, pagesCount, author);
+        this.type = 'novel';
+    }
+}
+
+
+class FantasticBook extends Book {
+    constructor(name, releaseDate, pagesCount, author) {
+        super(name, releaseDate, pagesCount, author);
+        this.type = 'fantastic';
+    }
+}
+
+
+class DetectiveBook extends Book {
+    constructor(name, releaseDate, pagesCount, author) {
+        super(name, releaseDate, pagesCount, author);
+        this.type = 'detective';
+    }
+}
+
+
+// -----------------L I B R A R Y
+
+class Library {
+    constructor(name) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if(book.state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBookBy(key, value) {
+        let findedBook = this.books.find( e => e[key] === value);
+
+        if(findedBook) {
+            return findedBook;
+        }
+        else {
+            return null;
+        }
+    }
+
+    giveBookByName(bookName) {
+        let findedBook = this.books.find( e => e.name === bookName);
+
+        if(findedBook) {
+            this.books.splice(this.books.indexOf(findedBook), 1);
+            return findedBook;    
+        }
+        else{
+            return null;
+        }
+    }
+}
+
+
+// -------------------S T U D E N T S
+
+
+class Student {
+    constructor(name){
+        this.name = name;
+        this.journal = [];
+    }
+
+
+    addMark(mark, subject) {
+        if(mark < 1 || mark > 5) {
+            console.log('Ошибка: оценка не соответствует устанвленной шкале оценок')
+            return 'Ошибка: оценка не соответствует установленной шкале оценок'
+        }
+        else if(this?.journal) {
+            this.journal.subject = mark;
+        }
+        else {
+            this.journal.push(subject = [mark]);
+        }
+    }
+        
+};
+
+
+// const studentFirst = new Student('Svetlana', 'woman', 25);
+// const studentSecond = new Student('Igor', 'man', 23);
+// const studentThird = new Student('Oleg', 'man', 27);
+// const studentFourth = new Student('Irina', 'woman', 22);
+
+
+// debugger;
