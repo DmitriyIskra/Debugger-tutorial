3a6839f03360603bdda2233e9886f82fc1744fa5
Dmitriy Iskra <dmitriyiskra@mail.ru>
2022-11-28 22:03:04 +0300

    Completed task2 in task-decorators

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -14,10 +14,39 @@ function cachingDecoratorNew(func) {
     return "Вычисляем: " + result;
   }
 }
-debugger;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+
+function debounceDecoratorNew(func, delay) {
+  let timeoutId = null;
+
+  function wrapper(...args) { 
+    if(timeoutId === null) {
+      timeoutId = 1;
+      wrapper.count += 1;
+      wrapper.allCount += 1;
+      return func(...args);
+    }
+    else{
+      if(timeoutId) {
+        clearTimeout(timeoutId);
+      };
+  
+  
+      timeoutId = setTimeout(() => {
+        // timeoutId = null;
+        wrapper.count += 1;
+        return func(...args);
+      }, delay);
+      
+      wrapper.allCount += 1;  
+    }
+  }
+
+  wrapper.count = 0;
+  wrapper.allCount = 0;
+  
+
+  return wrapper;
 }
 
-// debugger;
+debugger;
